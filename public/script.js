880374ceeef9b3f0c21c8399e0820ead589ed6db
Vinesgusto <vinesgusto9@gmail.com>
2026-03-07 00:20:18 -0300

    update adress + list

--- public/script.js
+++ public/script.js
@@ -6,9 +6,21 @@ const cartTotal = document.getElementById("cart-total");
 const checkoutBtn = document.getElementById("checkout-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
 const cartCount = document.getElementById("cart-count");
+const nameInput = document.getElementById("name");
+const nameWarn = document.getElementById("name-warn");
 const adressInput = document.getElementById("address");
 const adressWarn = document.getElementById("address-warn");
+const bairroInput = document.getElementById("neighborhood");
+const bairroWarn = document.getElementById("address-warn");
+const cityInput = document.getElementById("city");
+const cityWarn = document.getElementById("city-warn");
+const numberHouseInput = document.getElementById("house-number");
+const numberHouseWarn = document.getElementById("numberhouse-warn");
+const referenceInput = document.getElementById("point-reference");
+const phoneInput = document.getElementById("number-phone");
+const phoneWarn = document.getElementById("phone-warn");
 const btnRemove = document.getElementById("remove-btn");
+const redBtn = document.getElementById("red-btn");
 
 let cart = [];
 
@@ -16,16 +28,22 @@ let cart = [];
 cartBtn.addEventListener("click", function () {
   updateCartModal();
   cartModal.style.display = "flex";
+  cartBtn.style.display = "none";
+  redBtn.style.display = "none";
 });
 
 cartModal.addEventListener("click", function (event) {
   if (event.target === cartModal) {
     cartModal.style.display = "none";
+    cartBtn.style.display = "flex";
+    redBtn.style.display = "flex";
   }
 });
 
 closeModalBtn.addEventListener("click", function () {
   cartModal.style.display = "none";
+  cartBtn.style.display = "flex";
+  redBtn.style.display = "flex";
 });
 
 menu.addEventListener("click", function (event) {
@@ -92,7 +110,7 @@ function updateCartModal() {
     currency: "BRL",
   });
 
-  cartCount.innerHTML = cart.length;
+  cartCount.innerHTML = cart.reduce((acc, item) => acc + item.quantity, 0);
 }
 
 // Deleção item Carrinho
@@ -120,13 +138,54 @@ function removeItemCart(name) {
     updateCartModal();
   }
 }
-
 adressInput.addEventListener("input", function (event) {
   let inputValue = event.target.value;
 
   if (inputValue !== "") {
     adressInput.classList.remove("border-red-500");
     adressWarn.classList.add("hidden");
+  }
+});
+nameInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    nameInput.classList.remove("border-red-500");
+    nameWarn.classList.add("hidden");
+  }
+});
+bairroInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    bairroInput.classList.remove("border-red-500");
+    bairroWarn.classList.add("hidden");
+  }
+});
+cityInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    cityInput.classList.remove("border-red-500");
+    cityWarn.classList.add("hidden");
+  }
+});
+
+numberHouseInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    numberHouseInput.classList.remove("border-red-500");
+    numberHouseWarn.classList.add("hidden");
+  }
+});
+
+phoneInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    phoneInput.classList.remove("border-red-500");
+    phoneWarn.classList.add("hidden");
   }
 });
 
@@ -137,41 +196,116 @@ checkoutBtn.addEventListener("click", function () {
   //   return;
   // }
 
+  let orderNumber = localStorage.getItem("orderNumber");
+
+  if (!orderNumber) {
+    orderNumber = 1;
+  } else {
+    orderNumber = parseInt(orderNumber) + 1;
+  }
+
+  orderNumber = String(orderNumber).padStart(3, "0");
+
+  localStorage.setItem("orderNumber", orderNumber);
+
   if (cart.length === 0) return;
-  if (adressInput.value === "") {
+  if (
+    adressInput.value |
+    nameInput.value |
+    bairroInput.value |
+    cityInput.value |
+    numberHouseInput.value |
+    (phoneInput.value === "")
+  ) {
     adressWarn.classList.remove("hidden");
     adressInput.classList.add("border-red-500");
+    nameWarn.classList.remove("hidden");
+    nameInput.classList.add("border-red-500");
+    bairroWarn.classList.remove("hidden");
+    bairroInput.classList.add("border-red-500");
+    cityWarn.classList.remove("hidden");
+    cityInput.classList.add("border-red-500");
+    numberHouseWarn.classList.remove("hidden");
+    numberHouseInput.classList.add("border-red-500");
+    phoneWarn.classList.remove("hidden");
+    phoneInput.classList.add("border-red-500");
     return;
   }
 
-  const cartItems = cart
-    .map((item) => {
-      return `${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |`;
-    })
-    .join("");
+  let total = 0;
+  cart.forEach((item) => {
+    total += item.price * item.quantity;
+  });
 
-  const message = encodeURIComponent(cartItems);
+  const cartItems = cart
+    .map((item, index) => {
+      return `${index + 1}️⃣ ${item.name}
+Qtd: ${item.quantity}
+Preço: R$ ${item.price}`;
+    })
+    .join("\n\n");
+
+  const message = `
+🧾 *NOVO PEDIDO ${orderNumber}*
+
+Cliente : ${nameInput.value}
+
+━━━━━━━━━━━━━━━
+🍔 *ITENS DO PEDIDO*
+
+${cartItems}
+
+━━━━━━━━━━━━━━━
+💰 *TOTAL*
+R$ ${total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })}
+
+━━━━━━━━━━━━━━━
+📍 *ENDEREÇO*
+*Rua:* ${adressInput.value}
+*Bairro:* ${bairroInput.value}
+*Cidade:* ${cityInput.value}
+*Número da Casa:* ${numberHouseInput.value}
+*Ponto de Referência:* ${referenceInput.value}
+*Contato:* ${phoneInput.value}
+
+━━━━━━━━━━━━━━━
+💳 *PAGAMENTO*
+ PIX
+
+━━━━━━━━━━━━━━━
+📝 *DESCRIÇÃO*
+
+`;
+
+  // const cartItems = cart
+  //   .map((item) => {
+  //     return `${item.name}\n Quantidade: (${item.quantity})\nPreço: R$${item.price}\n`;
+  //   })
+  //   .join("\n");
+
+  // const message = encodeURIComponent(cartItems);
+  const encodedMessage = encodeURIComponent(message);
   const phone = "5522998553582";
 
-  window.open(
-    `https://wa.me/${phone}?text=${message} Endereço: ${adressInput.value}`,
-    "_blank",
-  );
+  window.open(`https://wa.me/${phone}?text=${encodedMessage}`, "_blank");
 });
 
-function checkOpen() {
-  const data = new Date();
-  const hora = data.getHours();
-  return hora >= 18 && hora < 22;
-}
+// function checkOpen() {
+//   const data = new Date();
+//   const hora = data.getHours();
+//   return hora >= 18 && hora < 22;
+// }
 
-const spanItem = document.getElementById("date-span");
-const isOpen = checkOpen();
+// const spanItem = document.getElementById("date-span");
+// const isOpen = checkOpen();
 
-if (isOpen) {
-  spanItem.classList.remove("bg-red-500");
-  spanItem.classList.add("bg-green-600");
-} else {
-  spanItem.classList.remove("bg-green-600");
-  spanItem.classList.add("bg-red-500");
-}
+// if (isOpen) {
+//   spanItem.classList.remove("bg-red-500");
+//   spanItem.classList.add("bg-green-600");
+// } else {
+//   spanItem.classList.remove("bg-green-600");
+//   spanItem.classList.add("bg-red-500");
+// }
